6985fbb90cec0385db22bc4d8b3196c24810487e
unknown <tplirs1@adispo-437.diadyktio.local>
2014-04-29 08:00:14 +0300

    trying custom field to data type conversion

--- src/Main.hs
+++ src/Main.hs
@@ -25,11 +25,12 @@ import System.Exit (exitFailure)
 import Control.Applicative
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
+import Database.SQLite.Simple.FromField
 
 import Data.Time.Clock
 
 data Person = Person { idPerson :: Id, surname :: String, firstname :: String }
-data Shift = Shift { idShift :: Id, name :: String, period :: Period }
+data Shift = Shift { name :: String, period :: Period }
 data Counter = Counter { idCounter :: Id, personId :: Id, shiftId :: Id, value :: Int }
 data Period = Period UTCTime UTCTime
 
@@ -43,17 +44,23 @@ instance Show Counter where
     show (Counter c p s v) = show c ++ show p ++ show s ++ show v
 
 instance FromRow Person where
-  fromRow = Person <$> field <*> field <*> field
+    fromRow = Person <$> field <*> field <*> field
+
+instance FromRow Shift where
+    fromRow = Shift <$> field <*> Period
+
+instance FromField Period where
+    fromField p = Period <$> fromField p <*> field
 
 instance FromRow Counter where
-  fromRow = Counter <$> field <*> field <*> field <*> field
+    fromRow = Counter <$> field <*> field <*> field <*> field
 
 main :: IO ()
 main = do
   conn <- open "dist/resources/test.db"
   r <- query_ conn "select * from person" :: IO [Person]
-  --r <- query_ conn "select * from shift" :: IO [Shift]
-  r <- query_ conn "select * from counter" :: IO [Counter]
+  r <- query_ conn "select * from shift" :: IO [Shift]
+--  r <- query_ conn "select * from counter" :: IO [Counter]
   mapM_ print r
   close conn
 
