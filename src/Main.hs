cbc29049d0d372d1074f1bdab800b8353d5002ea
Stavros Polymenis <sp@orbitalfox.com>
2014-05-04 12:37:35 +0300

    implemented inserts, finished shift loading

--- src/Main.hs
+++ src/Main.hs
@@ -31,18 +31,15 @@ import Database.SQLite.Simple.Time.Implementation
 import Data.Time.Clock
 import Data.Time.LocalTime (TimeOfDay)
 
-data Person = Person { idPerson :: Id, surname :: String, firstname :: String }
-data Shift = Shift { name :: String, start :: UTCTime, end :: UTCTime}
-data Counter = Counter { idCounter :: Id, personId :: Id, shiftId :: String, value :: Int }
-
-type Counters = [Counter]
-type Id = Int
+data Person = Person { idPerson :: Int, surname :: String, first :: String }
+data Shift = Shift { name :: String, start :: String, end :: String}
+data Counter = Counter { idCounter :: Int, personId :: Int, shiftId :: String, value :: Int }
 
 instance Show Person where
     show (Person i s _) = (show i) ++ "\t" ++ s
 
 instance Show Shift where
-    show (Shift n s e) = n ++ "\t" ++ (show s) ++ ":" ++ (show e)
+    show (Shift n s e) = n ++ "\t" ++ (show (read s :: TimeOfDay)) ++ ":" ++ (show (read s :: TimeOfDay))
 
 instance Show Counter where
     show (Counter c p s v) = show c ++ show p ++ show s ++ show v
@@ -51,13 +48,17 @@ instance FromRow Person where
     fromRow = Person <$> field <*> field <*> field
 
 instance FromRow Shift where
-    fromRow = Shift <$> field <*> TimeOfDay <*> TimeOfDay
+    fromRow = Shift <$> field <*> field <*> field
 
 instance FromRow Counter where
     fromRow = Counter <$> field <*> field <*> field <*> field
 
-instance FromField TimeOfDay where
-    fromField = timeOfDayToBuilder
+insertPerson conn id surname first = execute conn "insert into person (id,surname,first) values (?,?,?)"
+							(id,surname, first)
+
+insertShift :: Connection -> String -> String -> String -> IO ()
+insertShift conn name start end =
+    execute conn "insert into shift (name,start,end) values (?,?,?)" (name,(show (read start::TimeOfDay)),(show (read end::TimeOfDay)))
 
 main :: IO ()
 main = do
